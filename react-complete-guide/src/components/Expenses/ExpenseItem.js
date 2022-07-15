e13adf4fad058e3c7510150043965f65e441c85c
김지형 <jihyeong@gimjihyeong-ui-MacBookAir.local>
2022-07-15 20:30:07 +0900

    FEAT : components, UI 폴더화

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 //매개변수는 props 하나만 받음. Properties에 모든 정보들이 저장되어 있는 상태
