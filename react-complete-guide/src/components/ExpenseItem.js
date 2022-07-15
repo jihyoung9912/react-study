9637ec8a12f4071e6f328cf53dee0274e683ee81
김지형 <jihyeong@gimjihyeong-ui-MacBookAir.local>
2022-07-15 20:04:14 +0900

    FEAT : Card.js

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 //매개변수는 props 하나만 받음. Properties에 모든 정보들이 저장되어 있는 상태
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
