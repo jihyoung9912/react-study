07c3653b9562c862931f8849ce71eb5c28b4e724
김지형 <jihyoung9912@gmail.com>
2022-08-25 22:10:26 +0900

    feat: TypeScript Practice

--- typescript/src/index.tsx
+++ typescript/src/index.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {ThemeProvider} from "styled-components";
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './theme';
 
-const darkTheme = {
-    textColor:"whitesmoke",
-    backgroundColor:'#111'
-}
-
-const lightTheme = {
-    textColor:"111",
-    backgroundColor:'whitesmoke'
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <ThemeProvider theme={lightTheme}>
-            <App/>
-        </ThemeProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
