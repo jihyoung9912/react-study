9658bc4736de8d3953b372df4a7d88b57a8a4a0e
김지형 <jihyoung9912@gmail.com>
2022-11-18 19:27:49 +0900

    feat: ExpenseList

--- react-fundamental/src/components/Expenses/ExpensesList.js
+++ react-fundamental/src/components/Expenses/ExpensesList.js
@@ -1,5 +1,20 @@
-const ExpensesList = () => {
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css"
 
+const ExpensesList = (props) => {
+    let expensesContent = <h2 className='expenses-list__fallback'>No expenses found</h2>
+
+    if (props.items.length === 0) {
+        return expensesContent
+    }
+
+    if (props.items.length > 0) {
+
+        return <ul className="expense-list">
+            {props.items.map((expense) => (
+                <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))}
+        </ul>
+    }
 }
 
 export default ExpensesList;
