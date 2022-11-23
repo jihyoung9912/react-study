cd851c1c01263901279861eef20df635b998ca60
김지형 <jihyoung9912@gmail.com>
2022-11-23 15:40:36 +0900

    feat: UsersList

--- First-Proj/src/App.js
+++ First-Proj/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import AddUser from "./Componenets/Users/AddUser";
+import UsersList from "./Componenets/Users/UsersList";
 
 
 function App() {
-    return (
-        <div>
-            <AddUser/>
-        </div>
-    );
+  return (
+    <div>
+      <AddUser/>
+      <UsersList users={[]}/>
+    </div>
+  );
 }
 
 export default App;
