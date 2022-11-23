3f88d1abc283eaad4c31e5673816a5bff33e39da
김지형 <jihyoung9912@gmail.com>
2022-11-23 21:22:02 +0900

    fix: name error

--- First-Proj/src/Components/Users/UsersList.js
+++ First-Proj/src/Components/Users/UsersList.js
@@ -5,7 +5,7 @@ const UsersList = props => {
   return (
     <Card clssName={classes.users}>
       <ul>
-        {props.users.map(user => <li>{user.name} ({user.age} years old)</li>)}
+        {props.users.map(user => <li key={user.id}>{user.name} ({user.age} years old)</li>)}
       </ul>
     </Card>
   )
